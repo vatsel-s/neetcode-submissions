6034e070078f94af09ea31a5cac894ea376a8b0e
Vatsel Srivastava <vatsels2@illinois.edu>
2026-07-20 21:23:37 -0700

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> arr(s.size()); 
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(arr[i] == 1 || i == 0)
+            {
+                for(int j = 0; j < wordDict.size(); j++)
+                {
+                    int len = wordDict[j].size(); 
+                    cout << len << " " << wordDict[j] << " " << i << " " << s.substr(i, i + len) << endl; 
+                    if(s.substr(i, len) == wordDict[j])
+                    {
+                        if(i + len == s.size())
+                        {
+                            return true; 
+                        }
+                        else if(i + len < s.size())
+                        {
+                            arr[i + len] = 1; 
+                            cout << i + len << endl; 
+                        } 
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+};
